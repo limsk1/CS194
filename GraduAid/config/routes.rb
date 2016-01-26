e224021edebdfad783afa995c74c43eade3e6e4e
limsk1 <limsk1@stanford.edu>
2016-01-25 20:55:05 -0800

    made a login page

--- GraduAid/config/routes.rb
+++ GraduAid/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'user/major'
+
+  get 'user/new'
+
+  get 'user/login'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'user#login'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
