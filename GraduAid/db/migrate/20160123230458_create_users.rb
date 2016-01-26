f73d644ccdaaa4872f0814d9ce8bbb376720fdef
limsk1 <limsk1@stanford.edu>
2016-01-25 20:55:05 -0800

    change root, add most basic controller

--- GraduAid/db/migrate/20160123230458_create_users.rb
+++ GraduAid/db/migrate/20160123230458_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.column :first_name, :string
       t.column :last_name,  :string
       t.column :password_digest, :string
-      t.column :salt        :string
+      t.column :salt,       :string
       t.timestamps
     end
   end
