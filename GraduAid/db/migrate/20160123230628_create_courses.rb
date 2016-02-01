c6a9309cb93d9c42992aef044b654a29313187b9
limsk1 <limsk1@stanford.edu>
2016-01-31 17:09:23 -0800

    add one line

--- GraduAid/db/migrate/20160123230628_create_courses.rb
+++ GraduAid/db/migrate/20160123230628_create_courses.rb
@@ -2,6 +2,7 @@ class CreateCourses < ActiveRecord::Migration
   def up
     create_table :courses do |t|
       t.column :course_name,    :string
+      t.column :general_req,    :string
       t.timestamps
     end
   end
