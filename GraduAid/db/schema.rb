42664ae27976f066eba09666a4192d70e827c547
limsk1 <limsk1@stanford.edu>
2016-02-07 03:56:48 -0800

    more dummy data...can compare better

--- GraduAid/db/schema.rb
+++ GraduAid/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206212352) do
+ActiveRecord::Schema.define(version: 20160207114749) do
 
   create_table "categories", force: true do |t|
     t.integer  "track_id"
