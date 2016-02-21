9fd3ec887d69c2d079fef903ba01a3e8a27872dc
jipark21 <jipark21@stanford.edu>
2016-02-20 17:50:56 -0800

    log in css improved

--- GraduAid/config/initializers/assets.rb
+++ GraduAid/config/initializers/assets.rb
@@ -6,8 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-<<<<<<< HEAD
-=======
+#<<<<<<< HEAD
+#=======
 
->>>>>>> 42664ae27976f066eba09666a4192d70e827c547
+#>>>>>>> 42664ae27976f066eba09666a4192d70e827c547
 Rails.application.config.assets.precompile += %w( user.css )
