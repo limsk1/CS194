bdbbf122c4dd8ae515d3aee976362eb991f3b7e3
limsk1 <limsk1@stanford.edu>
2016-02-29 01:41:50 -0800

    move lines to support past version

--- GraduAid/config/application.rb
+++ GraduAid/config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.enabled = true  
-config.assets.paths << "#{Rails.root}/app/assets/fonts"  
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +19,9 @@ module GraduAid
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.enabled = true  
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"  
+
   end
 end
